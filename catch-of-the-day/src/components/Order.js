5af5ff58b7850195ec37617cdbec484c29bfc3dd
Will Stone <willstone@Wills-MacBook-Pro.local>
2020-04-14 12:19:04 -0400

    added order total

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { formatPrice } from "../helpers"
 
 class Order extends React.Component {
     render() {
+        const orderIds = Object.keys(this.props.order);
+        const total = orderIds.reduce((total, key) => {
+            const fish = this.props.fishes[key];
+            const count = this.props.order[key];
+            const isAvailable = fish && fish.status === "available";
+            if (isAvailable){
+                return total + (count * fish.price);
+            }
+            return total;
+        }, 0);
         return (
-            <div className="Order">Order!!!</div>
+            <div className="order-wrap">
+                <h2>Order</h2>
+                <div className="total">
+                    Total: 
+                    <strong>{formatPrice(total)}</strong>
+                </div>
+            </div>
         )
     }
 }
